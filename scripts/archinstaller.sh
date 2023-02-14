6b95ce1c96481163eb71ae36b29de18ed363d37b
matt1432 <matt@nelim.org>
2023-02-14 11:15:49 -0500

    Separeted scripts

--- scripts/archinstaller.sh
+++ scripts/archinstaller.sh
@@ -18,27 +18,4 @@ pacman -Syu
 pacstrap -K /mnt base linux-firmware linux amd-ucode patch dkms kmod rtw89-dkms-git btrfs-progs grub os-prober ntfs-3g efibootmgr efivar iwd nano sudo texinfo man-db man-pages
 
 genfstab -U /mnt >> /mnt/etc/fstab
-
-### TODO separate in 2 scripts but remember $PART
-
 arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/America/Montreal /etc/localtime
-hwclock --systohc
-echo matt-laptop > /etc/hostname
-passwd
-
-nano /etc/locale.gen
-locale-gen
-echo LANG=en_CA.UTF-8 > /etc/locale.conf
-echo KEYMAP=ca > /etc/vconsole.conf
-
-nano /etc/mkinitcpio.conf # TODO SED
-mkinitcpio -P
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch
-
-CRYPT="cryptdevice=$(blkid | sed -n 's/.*nvme0n1p'$PART': \(.*\) TYPE.*/\1/p'):root"
-sed -i 's#GRUB_CMDLINE_LINUX_DEFAULT.*#GRUB_CMDLINE_LINUX_DEFAULT="quiet loglevel 3 '$CRYPT' root=/dev/mapper/root"#' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo REBOOT
