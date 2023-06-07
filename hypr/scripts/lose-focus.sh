a4d4c15a91dc342adae811068761f7448caa9e65
matt1432 <matt@nelim.org>
2023-06-07 15:27:51 -0400

    fix: redirect lose-focus output to /dev/null

--- hypr/scripts/lose-focus.sh
+++ hypr/scripts/lose-focus.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	while killall -0 blueman-manager
+	while killall -0 blueman-manager > /dev/null 2>&1
 	do
 		if [[ $(bluetoothctl show | grep Powered | grep yes) ]]; then
 			if [[ $(hyprctl activewindow | grep blueman-manager) == "" && $(hyprctl clients | grep blueman-manager) != "" ]]; then
