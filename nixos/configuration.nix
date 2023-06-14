726e069236c445e9173cb0c8461fcc2172b4e459
matt1432 <matt@nelim.org>
2023-06-13 23:36:06 -0400

    fix: remove ambiguous Unicode characters

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -195,7 +195,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.matt = {
     isNormalUser = true;
     extraGroups = [ "wheel" "input" "adm" ];
