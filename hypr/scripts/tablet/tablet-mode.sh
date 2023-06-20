54bf36a1daed91d9e8638551d1517083d6c87eff
matt1432 <matt@nelim.org>
2023-06-19 20:51:55 -0400

    feat: buncha stuff

--- hypr/scripts/tablet/tablet-mode.sh
+++ hypr/scripts/tablet/tablet-mode.sh
@@ -6,7 +6,7 @@ gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled true
 
 brightnessctl -d tpacpi::kbd_backlight s 0
 
-iio-hyprland &
+$HOME/.local/bin/iio-hyprland &
 
 evtest --grab "/dev/input/by-path/platform-i8042-serio-0-event-kbd" &
 evtest --grab "/dev/input/by-path/platform-i8042-serio-1-event-mouse" &
