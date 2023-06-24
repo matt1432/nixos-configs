d16051dada8ff661a5aa62f5cdb5a77b4250c51f
matt1432 <matt@nelim.org>
2023-06-24 12:35:40 +0200

    TODO: put all this in nixos configs

--- hypr/scripts/tablet/laptop-mode.sh
+++ hypr/scripts/tablet/laptop-mode.sh
@@ -5,5 +5,5 @@ gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled false
 
 brightnessctl -d tpacpi::kbd_backlight s 2
 
-killall iio-hyprland
-killall evtest
+killall -r autorotate.sh
+killall -r evtest
--- hypr/scripts/tablet/tablet-mode.sh
+++ hypr/scripts/tablet/tablet-mode.sh
@@ -6,7 +6,7 @@ gsettings set org.gnome.desktop.a11y.applications screen-keyboard-enabled true
 
 brightnessctl -d tpacpi::kbd_backlight s 0
 
-$HOME/.local/bin/iio-hyprland &
+$HOME/.config/hypr/scripts/autorotate.sh &
 
 evtest --grab "/dev/input/by-path/platform-i8042-serio-0-event-kbd" &
 evtest --grab "/dev/input/by-path/platform-i8042-serio-1-event-mouse" &
