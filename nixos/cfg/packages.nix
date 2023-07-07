bf1ab5d93a4a7535533422a2f793141926d8b4f3
matt1432 <matt@nelim.org>
2023-07-07 11:41:31 -0400

    feat: add dracula theme to git

--- nixos/cfg/packages.nix
+++ nixos/cfg/packages.nix
@@ -78,6 +78,54 @@
     git = { # TODO: make better config
       enable = true;
       lfs.enable = true;
+      config = {
+        # Dracula Dark Theme
+        color = {
+          ui = "auto";
+          branch = {
+            current = "cyan bold reverse";
+            local = "white";
+            plain = "";
+            remote = "cyan";
+          };
+          diff = {
+            commit = "";
+            func = "cyan";
+            plain = "";
+            whitespace = "magenta reverse";
+            meta = "white";
+            frag = "cyan bold reverse";
+            old = "red";
+            new = "green";
+          };
+          grep = {
+            context = "";
+            filename = "";
+            function = "";
+            linenumber = "white";
+            match = "";
+            selected = "";
+            separator = "";
+          };
+          interactive = {
+            error = "";
+            header = "";
+            help = "";
+            prompt = "";
+          };
+          status = {
+            added = "green";
+            changed = "yellow";
+            header = "";
+            localBranch = "";
+            nobranch = "";
+            remoteBranch = "cyan bold";
+            unmerged = "magenta bold reverse";
+            untracked = "red";
+            updated = "green bold";
+          };
+        };
+      };
     };
 
     htop = {
