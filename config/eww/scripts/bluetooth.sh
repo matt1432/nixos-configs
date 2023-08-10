96a81d7e5beda77f5c0df243aabbf83405d88a3d
matt1432 <matt@nelim.org>
2023-08-09 22:11:09 -0400

    feat(eww): make some scripts always run at start. make bluetooth state persistent

--- config/eww/scripts/bluetooth.sh
+++ config/eww/scripts/bluetooth.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+FILE="$HOME/.config/.bluetooth"
+
 get_device() {
   if [[ $(bluetoothctl devices Connected) != "" ]]; then
     bluetoothctl devices Connected | awk '{ for (i = 3; i <= NF; i++) { printf("%s ", $i) } printf("\n") }'
@@ -10,9 +12,9 @@ get_device() {
 
 get_state() {
   if [[ "$(rfkill list | grep -A 1 hci0 | grep -o no)" == "no" ]]; then
-    echo " 󰂯 "
+    echo " 󰂯 " > "$FILE"
   else
-    echo " 󰂲 " 
+    echo " 󰂲 " > "$FILE"
   fi
 }
 
@@ -23,5 +25,6 @@ if [[ $1 == "icon" ]]; then
   while true; do
     sleep 0.1
     get_state
+    tail "$FILE"
   done
 fi
--- config/eww/scripts/heart.sh
+++ config/eww/scripts/heart.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
 FILE="$HOME/.config/.heart"
-if [[ ! -f "$FILE" ]]; then
-  echo 󰣐 > "$FILE"
-fi
 
-if grep -q 󰣐 "$FILE"; then
-  eww close heart
-  eww update heart-ts=false
-  echo  > "$FILE"
-else
-  eww open heart
-  eww update heart-ts=true
-  echo 󰣐 >> "$FILE"
-fi
+toggle() {
+  if grep -q 󰣐 "$FILE"; then
+    eww close heart
+    eww update heart-ts=false
+    echo  > "$FILE"
+  else
+    eww open heart
+    eww update heart-ts=true
+    echo 󰣐 >> "$FILE"
+  fi
+}
+
+[[ "$1" == "toggle" ]] && toggle
+
+
+startup() {  
+  if [[ ! -f "$FILE" ]]; then
+    echo 󰣐 > "$FILE"
+  fi
+
+  if [[ ! -f "$HOME/.config/.bluetooth" ]]; then
+    echo 󰂲 > "$FILE"
+  fi
+
+  if grep -q 󰂲 "$HOME/.config/.bluetooth"; then
+    rfkill block bluetooth
+  fi
+
+  tail -f "$FILE"
+}
+
+[[ "$1" == "startup" ]] && startup
