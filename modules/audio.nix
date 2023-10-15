9942d9a0bcebbd9acf11690958b36a68a45add09
matt1432 <matt@nelim.org>
2023-10-15 17:22:22 -0400

    feat: add audio module

---
+++ modules/audio.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  hardware.pulseaudio.enable = false;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+    pulse.enable = true;
+  };
+}
