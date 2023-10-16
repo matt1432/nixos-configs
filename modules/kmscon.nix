1163a21e61457ad071466b3ae2b9cce7bb10995a
matt1432 <matt@nelim.org>
2023-10-15 23:23:23 -0400

    fix(kmscon): fix lag on nvidia card

--- modules/kmscon.nix
+++ modules/kmscon.nix
@@ -1,7 +1,7 @@
 { ... }: {
   services.kmscon = {
     enable = true;
-    hwRender = true;
+    hwRender = false;
     # FIXME: https://github.com/Aetf/kmscon/issues/18    // Icons not rendering properly
     # FIXME: https://github.com/Aetf/kmscon/issues/56    // Mouse cursor stays
     extraOptions = "--font-size 12.5 --font-dpi 170 --font-name 'JetBrainsMono Nerd Font'";
