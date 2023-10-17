00762bb2e90210276bf5e24bd953a68beb4fc574
matt1432 <matt@nelim.org>
2023-10-17 19:37:21 -0400

    feat(binto): add steam

--- hosts/binto/modules/nix-gaming.nix
+++ hosts/binto/modules/nix-gaming.nix
@@ -1,7 +1,19 @@
-# configuration.nix
-{
+{ nix-gaming, pkgs, ... }: {
   nix.settings = {
     substituters = ["https://nix-gaming.cachix.org"];
     trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+  };
+
+  imports = [
+    nix-gaming.nixosModules.steamCompat
+  ];
+
+  programs.steam = {
+    enable = true;
+
+    extraCompatPackages = [
+      # pkgs.luxtorpeda
+      nix-gaming.packages.${pkgs.system}.proton-ge
+    ];
   };
 }
