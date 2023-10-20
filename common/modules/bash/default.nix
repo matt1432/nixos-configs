4c180c599d59ae1a855e5f7e200c94f3f468298a
matt1432 <matt@nelim.org>
2023-10-20 13:24:30 -0400

    fix(ssh): use hostnames that tailscale understands

--- common/modules/bash/default.nix
+++ common/modules/bash/default.nix
@@ -135,14 +135,14 @@
 
         tup   = "tailscale up --login-server https://headscale.nelim.org";
 
-        pc    = "mosh matt@10.0.0.248 -- tmux -2u new -At laptop";
-        oksys = "mosh matt@10.0.0.213 -- tmux -2u new -At laptop";
-        pve   = "mosh matt@10.0.0.121 -- tmux -2u new -At laptop";
+        pc    = "mosh matt@binto -- tmux -2u new -At laptop";
+        oksys = "mosh matt@oksys -- tmux -2u new -At laptop";
+        pve   = "mosh matt@pve -- tmux -2u new -At laptop";
 
-        mc    = "mosh mc@10.0.0.124 -- tmux -2u new -At laptop";
-        pod   = "mosh matt@10.0.0.121 -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At laptop'";
-        jelly = "mosh matt@10.0.0.121 -- ssh -t matt@10.0.0.123 'tmux -2u new -At laptop'";
-        qbit  = "mosh matt@10.0.0.121 -- ssh -t matt@10.0.0.128 'tmux -2u new -At laptop'";
+        mc    = "mosh mc@mc -- tmux -2u new -At laptop";
+        pod   = "mosh matt@pve -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At laptop'";
+        jelly = "mosh matt@pve -- ssh -t matt@10.0.0.123 'tmux -2u new -At laptop'";
+        qbit  = "mosh matt@pve -- ssh -t matt@10.0.0.128 'tmux -2u new -At laptop'";
       };
       sessionVariables = {
         # FIXME: why is this not set by home-manager?
