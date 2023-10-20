a98fd6bdbd239a2460a5907220dc719114e8c552
matt1432 <matt@nelim.org>
2023-10-19 22:32:44 -0400

    refactor(greetd): separate configs a bit

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -8,11 +8,7 @@
   hyprBin = "${hyprland.packages.x86_64-linux.default}/bin";
   gset = pkgs.gsettings-desktop-schemas;
 
-  css = pkgs.writeText "style.css" ''
-    window {
-      background-color: rgba(0, 0, 0, 0);
-    }
-  '';
+  css = pkgs.writeText "style.css" ''${builtins.readFile ./style.css}'';
 
   paperConf = pkgs.writeText "hyprpaper.conf" ''
     preload = ${pkgs.dracula-theme}/wallpapers/waves.png
@@ -22,40 +18,7 @@
   hyprConf = pkgs.writeText "greetd-hypr-config" ''
     exec-once = hyprpaper --config ${paperConf}
 
-    input {
-        kb_layout = ca
-        kb_variant = multix
-        kb_model =
-        kb_options =
-        kb_rules =
-
-        follow_mouse = 1
-
-        touchpad {
-            natural_scroll = yes
-        }
-        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-    }
-
-    env = XCURSOR_SIZE,24
-    exec-once=hyprctl setcursor Dracula-cursors 24
-
-    misc {
-        disable_hyprland_logo = true
-        disable_splash_rendering = true
-        vfr = true
-    }
-
-    general {
-        border_size = 0
-    }
-
-    decoration {
-        blur {
-            enabled = false
-        }
-        drop_shadow = false
-    }
+    ${builtins.readFile ./hyprland.conf}
 
     # FIXME: kb doesn't work
     env = XDG_DATA_DIRS, ${gset}/share/gsettings-schemas/${gset.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
