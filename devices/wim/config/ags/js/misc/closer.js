7f06a60027f4d1e9f4474c93c651bfa12b632dbc
matt1432 <matt@nelim.org>
2023-10-28 21:43:40 -0400

    fix(ags closer): close all popup windows

--- devices/wim/config/ags/js/misc/closer.js
+++ devices/wim/config/ags/js/misc/closer.js
@@ -5,11 +5,9 @@ import Pointers from '../../services/pointers.js';
 
 
 export default () => {
-    const closableWindows = Array.from(App.windows).filter(w => {
-        return w[1].closeOnUnfocus &&
-               w[1].closeOnUnfocus !== 'none';
-    });
-    closableWindows.forEach(w => {
-        App.closeWindow(w[0]);
-    });
+    Array.from(App.windows)
+        .filter(w => w[1].closeOnUnfocus)
+        .forEach(w => {
+            App.closeWindow(w[0]);
+        });
 };
