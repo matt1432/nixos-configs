aaea660e4523882a86ebc508e7b851666e0eeb88
matt1432 <matt@nelim.org>
2023-11-19 19:16:18 -0500

    refactor(ags): add css option to some custom widgets

--- devices/wim/config/ags/js/misc/separator.js
+++ devices/wim/config/ags/js/misc/separator.js
@@ -1,16 +1,16 @@
 import { Box } from 'resource:///com/github/Aylur/ags/widget.js';
 
 
-export default (size, { vertical = false, ...props } = {}) => {
+export default (size, { vertical = false, css = '', ...props } = {}) => {
     if (vertical) {
         return Box({
-            css: `min-height: ${size}px;`,
+            css: `min-height: ${size}px; ${css}`,
             ...props,
         });
     }
     else {
         return Box({
-            css: `min-width: ${size}px;`,
+            css: `min-width: ${size}px; ${css}`,
             ...props,
         });
     }
