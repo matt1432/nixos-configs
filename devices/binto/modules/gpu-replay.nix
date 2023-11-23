9f1a672f6578ff5092f241de4a171af9dcd832dc
matt1432 <matt@nelim.org>
2023-11-23 01:07:56 -0500

    refactor(gsr): update and drop systemd file

--- devices/binto/modules/gpu-replay.nix
+++ devices/binto/modules/gpu-replay.nix
@@ -6,8 +6,8 @@
   gsr = pkgs.gpu-screen-recorder.overrideAttrs (o: {
     src = pkgs.fetchgit {
       url = "https://repo.dec05eba.com/gpu-screen-recorder";
-      rev = "1ac862d155e10f050e6f6cca5381f9f5a3528d98";
-      hash = "sha256-wLiBn4VIi+IWY4qVkdFzkHhECTFH97snogVTEwM5yx8=";
+      rev = "2f60f4c109f1bcc748d5f0d937476e8708a4073b";
+      hash = "sha256-pdyKspXx6ozXLgfLHAyRrPVJ2kzllHQDhPu1QQwpQjU=";
     };
 
     buildInputs =
@@ -29,7 +29,7 @@
 
       install -Dm755 "gsr-kms-server" "$out/bin/gsr-kms-server"
       install -Dm755 "gpu-screen-recorder" "$out/bin/gpu-screen-recorder"
-      install -Dm644 "extra/gpu-screen-recorder.service" "$out/lib/systemd/user/gpu-screen-recorder.service"
+      #install -Dm644 "extra/gpu-screen-recorder.service" "$out/lib/systemd/user/gpu-screen-recorder.service"
 
       wrapProgram $out/bin/gpu-screen-recorder --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [
         pkgs.addOpenGLRunpath.driverLink
@@ -44,11 +44,6 @@ in {
 
     (writeShellScriptBin "gpu-save-replay" ''
       exec ${pkgs.procps}/bin/pkill --signal SIGUSR1 -f gpu-screen-recorder
-    '')
-
-    # Run this after login to make sure it works
-    (writeShellScriptBin "gpu-restart-replay" ''
-      exec systemctl --user restart gpu-screen-recorder.service
     '')
   ];
 
