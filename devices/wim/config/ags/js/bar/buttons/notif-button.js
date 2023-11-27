d8881b9eca2d8239d41fa18adae21e963219a9a6
matt1432 <matt@nelim.org>
2023-11-27 15:15:02 -0500

    fix(wim ags): fix notif button

--- devices/wim/config/ags/js/bar/buttons/notif-button.js
+++ devices/wim/config/ags/js/bar/buttons/notif-button.js
@@ -1,9 +1,12 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Notifications from 'resource:///com/github/Aylur/ags/service/notifications.js';
 
-import { Box, Icon, Label } from 'resource:///com/github/Aylur/ags/widget.js';
+import { Box, CenterBox, Icon, Label } from 'resource:///com/github/Aylur/ags/widget.js';
 
 import EventBox from '../../misc/cursorbox.js';
+import Separator from '../../misc/separator.js';
+
+const SPACING = 4;
 
 
 export default () => EventBox({
@@ -17,30 +20,32 @@ export default () => EventBox({
         }
     }]],
 
-    child: Box({
+    child: CenterBox({
         className: 'notif-panel',
-        vertical: false,
 
-        children: [
-            Icon({
-                hpack: 'center',
-                connections: [[Notifications, (self) => {
-                    if (Notifications.dnd) {
-                        self.icon = 'notification-disabled-symbolic';
-                    }
-                    else if (Notifications.notifications.length > 0) {
-                        self.icon = 'notification-new-symbolic';
-                    }
-                    else {
-                        self.icon = 'notification-symbolic';
-                    }
-                }]],
-            }),
-            Label({
-                binds: [['label', Notifications, 'notifications',
-                    (n) => String(n.length)]],
-            }),
+        center_widget: Box({
+            children: [
+                Icon({
+                    connections: [[Notifications, (self) => {
+                        if (Notifications.dnd) {
+                            self.icon = 'notification-disabled-symbolic';
+                        }
+                        else if (Notifications.notifications.length > 0) {
+                            self.icon = 'notification-new-symbolic';
+                        }
+                        else {
+                            self.icon = 'notification-symbolic';
+                        }
+                    }]],
+                }),
 
-        ],
+                Separator(SPACING),
+
+                Label({
+                    binds: [['label', Notifications, 'notifications',
+                        (n) => String(n.length)]],
+                }),
+            ],
+        }),
     }),
 });
