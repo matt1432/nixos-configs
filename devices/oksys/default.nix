2d950fe54190853eb47fb36df981220f2b8ec180
matt1432 <matt@nelim.org>
2023-11-28 21:41:52 -0500

    feat: prepare oksys

---
+++ devices/oksys/default.nix
@@ -0,0 +1,40 @@
+{...}: {
+  imports = [
+  ];
+
+  services.device-vars = {
+    username = "matt";
+  };
+
+  users.users.matt = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "adm"
+      "mlocate"
+    ];
+  };
+  home-manager.users = {
+    matt = {
+      imports = [];
+
+      # No touchy
+      home.stateVersion = "24.05";
+    };
+  };
+
+  networking = {
+    hostName = "oksys";
+    networkmanager = {
+      enable = true;
+      wifi.backend = "wpa_supplicant";
+    };
+    firewall.enable = false;
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Montreal";
+
+  # No touchy
+  system.stateVersion = "24.05";
+}
