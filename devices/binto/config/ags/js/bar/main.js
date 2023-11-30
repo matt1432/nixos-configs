d48403d9c8d64216b3e8104401434ae024b5b0e6
matt1432 <matt@nelim.org>
2023-11-30 14:13:01 -0500

    feat(binto ags bar): make spacing consistent with hyprland

--- devices/binto/config/ags/js/bar/main.js
+++ devices/binto/config/ags/js/bar/main.js
@@ -1,8 +1,12 @@
 import { Box, CenterBox, Window } from 'resource:///com/github/Aylur/ags/widget.js';
 
 import SysTray from 'file:///home/matt/.nix/devices/wim/config/ags/js/bar/buttons/systray.js';
+import Separator from 'file:///home/matt/.nix/devices/wim/config/ags/js/misc/separator.js';
 import NotifButton from 'file:///home/matt/.nix/devices/wim/config/ags/js/bar/buttons/notif-button.js';
 import Clock from './buttons/clock.js';
+
+const PADDING = 20;
+
 
 export default () => Window({
     name: 'bar',
@@ -11,24 +15,32 @@ export default () => Window({
     anchor: ['bottom', 'left', 'right'],
     monitor: 1,
 
-    child: CenterBox({
-        start_widget: Box({
-            hpack: 'start',
-            children: [
-                SysTray(),
-            ],
-        }),
+    child: Box({
+        vertical: true,
+        children: [
+            CenterBox({
+                start_widget: Box({
+                    hpack: 'start',
+                    children: [
+                        Separator(PADDING),
+                        SysTray(),
+                    ],
+                }),
 
-        center_widget: Box({
-            hpack: 'center',
-        }),
+                center_widget: Box({
+                    children: [],
+                }),
 
-        end_widget: Box({
-            hpack: 'end',
-            children: [
-                NotifButton(),
-                Clock(),
-            ],
-        }),
+                end_widget: Box({
+                    hpack: 'end',
+                    children: [
+                        NotifButton(),
+                        Clock(),
+                        Separator(PADDING),
+                    ],
+                }),
+            }),
+            Separator(PADDING, {vertical: true}),
+        ],
     }),
 });
