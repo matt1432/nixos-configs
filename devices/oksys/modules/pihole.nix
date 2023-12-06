11c2b185b6df8f2353877bd3dfe103be0960df4e
matt1432 <matt@nelim.org>
2023-12-06 01:55:59 -0500

    chore: update flake.lock

--- devices/oksys/modules/pihole.nix
+++ devices/oksys/modules/pihole.nix
@@ -1,22 +1,32 @@
 {pihole, ...}: {
   imports = [pihole.nixosModules.default];
 
-  #TODO: set the whole thing up
   services.pihole = {
     enable = true;
 
-    dnsPort = 5335;
+    dnsPort = 5353;
     webPort = 8080;
 
     piholeConfig = {
       ftl = {
-        LOCAL_IPV4 = "127.0.0.1";
+        # Defaults
+        PRIVACYLEVEL = "0";
+        RATE_LIMIT = "1000/60";
       };
 
+      interface = "tailscale0";
+
       web = {
+        theme = "default-darker";
         virtualHost = "pi.hole";
         password = "password";
       };
+
+      # Handle it with unbound
+      dns.upstreamServers = [
+        "127.0.0.1#5335"
+        "127.0.0.1#5335"
+      ];
     };
   };
 }
