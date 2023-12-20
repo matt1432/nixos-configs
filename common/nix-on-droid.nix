6065c13c184978eeac1859220ff57c70b6494d4a
matt1432 <matt@nelim.org>
2023-12-20 12:51:33 -0500

    fix(droid): switch to tailscale ip for aliases

--- common/nix-on-droid.nix
+++ common/nix-on-droid.nix
@@ -43,14 +43,13 @@
 
       {
         programs.bash.shellAliases = {
-          pc = "mosh matt@binto -- tmux -2u new -At laptop";
-          oksys = "mosh matt@oksys -- tmux -2u new -At laptop";
-          pve = "mosh matt@pve -- tmux -2u new -At laptop";
+          pc = "mosh matt@100.64.0.6 -- tmux -2u new -At laptop";
+          oksys = "mosh matt@100.64.0.1 -- tmux -2u new -At laptop";
+          pve = "mosh matt@100.64.0.4 -- tmux -2u new -At laptop";
 
-          mc = "mosh mc@mc -- tmux -2u new -At laptop";
-          pod = "mosh matt@pve -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At laptop'";
-          jelly = "mosh matt@pve -- ssh -t matt@10.0.0.123 'tmux -2u new -At laptop'";
-          qbit = "mosh matt@pve -- ssh -t matt@10.0.0.128 'tmux -2u new -At laptop'";
+          pod = "mosh matt@100.64.0.4 -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At laptop'";
+          jelly = "mosh matt@100.64.0.4 -- ssh -t matt@10.0.0.123 'tmux -2u new -At laptop'";
+          qbit = "mosh matt@100.64.0.4 -- ssh -t matt@10.0.0.128 'tmux -2u new -At laptop'";
         };
       }
     ];
