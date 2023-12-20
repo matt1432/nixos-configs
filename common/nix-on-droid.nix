b887327642d465ec38846b87a32abecd64674b2b
Updater <robot@nowhere.invalid>
2023-12-20 04:07:56 -0500

    feat(droid): add ssh aliases

--- common/nix-on-droid.nix
+++ common/nix-on-droid.nix
@@ -40,6 +40,19 @@
 
       ./home
       ./pkgs
+
+      {
+        programs.bash.shellAliases = {
+          pc = "mosh matt@binto -- tmux -2u new -At laptop";
+          oksys = "mosh matt@oksys -- tmux -2u new -At laptop";
+          pve = "mosh matt@pve -- tmux -2u new -At laptop";
+
+          mc = "mosh mc@mc -- tmux -2u new -At laptop";
+          pod = "mosh matt@pve -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At laptop'";
+          jelly = "mosh matt@pve -- ssh -t matt@10.0.0.123 'tmux -2u new -At laptop'";
+          qbit = "mosh matt@pve -- ssh -t matt@10.0.0.128 'tmux -2u new -At laptop'";
+        };
+      }
     ];
 
     home.stateVersion = "23.05";
