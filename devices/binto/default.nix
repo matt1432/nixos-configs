594a9fd715150da473bceef58357fa1f10256dd1
matt1432 <matt@nelim.org>
2023-12-24 16:15:19 -0500

    feat(servers): add authorizedKeys to nix config

--- devices/binto/default.nix
+++ devices/binto/default.nix
@@ -33,6 +33,11 @@
       "video"
       "libvirtd"
     ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPE39uk52+NIDLdHeoSHIEsOUUFRzj06AGn09z4TUOYm matt@OP9"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGbLu+Gb7PiyNgNXMHemaQLnKixebx1/4cdJGna9OQp matt@wim"
+    ];
   };
   home-manager.users.${config.vars.user} = {
     imports = [
--- devices/servivi/default.nix
+++ devices/servivi/default.nix
@@ -17,6 +17,12 @@
       "wheel"
       "adm"
     ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPE39uk52+NIDLdHeoSHIEsOUUFRzj06AGn09z4TUOYm matt@OP9"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICr2+CpqXNMLsjgbrYyIwTKhlVSiIYol1ghBPzLmUpKl matt@binto"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGbLu+Gb7PiyNgNXMHemaQLnKixebx1/4cdJGna9OQp matt@wim"
+    ];
   };
   home-manager.users.${config.vars.user} = {
     imports = [];
