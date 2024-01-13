92fe82ab6ac76b34bf7f84e7eddca9228d511854
matt1432 <matt@nelim.org>
2024-01-13 16:09:58 -0500

    fix(ags wim): fix path to utils

--- devices/wim/config/ags/config.js
+++ devices/wim/config/ags/config.js
@@ -1,4 +1,4 @@
-import { watchAndCompileSass, transpileTypeScript } from 'js/utils';
+import { watchAndCompileSass, transpileTypeScript } from './js/utils.js';
 
 watchAndCompileSass();
 export default (await import(await transpileTypeScript())).default;
