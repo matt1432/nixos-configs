0d0e327980e4c8ae7731e57f6fa9e32a2916161a
matt1432 <matt@nelim.org>
2024-01-14 18:32:32 -0500

    fix(binto): fix build errors with nvidia-open

--- devices/binto/modules/nvidia.nix
+++ devices/binto/modules/nvidia.nix
@@ -18,7 +18,6 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-    # Modesetting is required.
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
@@ -27,20 +26,14 @@
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = true;
+    # FIXME: https://github.com/NVIDIA/open-gpu-kernel-modules/pull/589
+    open = false;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    # Vulkan is much more stable in Wayland
     package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 }
