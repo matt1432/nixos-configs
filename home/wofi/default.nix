948fbf943e5ef52fe61ee7d2a908f70933946141
matt1432 <matt@nelim.org>
2024-01-14 16:09:17 -0500

    chore: update flake.lock

--- home/wofi/default.nix
+++ home/wofi/default.nix
@@ -1,7 +1,13 @@
-{...}: {
+{
+  nixpkgs,
+  pkgs,
+  ...
+}: {
   programs = {
     wofi = {
       enable = true;
+      # FIXME: remove when overlay gets fixed
+      package = pkgs.callPackage "${nixpkgs}/pkgs/applications/misc/wofi/default.nix" {};
       settings = {
         prompt = "";
         allow_images = true;
