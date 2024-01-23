1cfbf188661416d5ad07e1af5a0e3ba3d1d5df11
matt1432 <matt@nelim.org>
2024-01-23 08:06:18 -0500

    feat(tailscale): add ssh aliases for cluster nodes

--- common/nix-on-droid.nix
+++ common/nix-on-droid.nix
@@ -47,11 +47,21 @@
 
       {
         programs.bash.shellAliases = {
+          # Desktop
           pc = "ssh -t matt@100.64.0.6 'tmux -2u new -At phone'";
+
+          # Misc Nix servers
           oksys = "ssh -t matt@100.64.0.1 'tmux -2u new -At phone'";
           servivi = "ssh -t matt@100.64.0.7 'tmux -2u new -At phone'";
+
+          # Cluster nodes
+          thingone = "ssh -t matt@100.64.0.8 'tmux -2u new -At phone'";
+          thingtwo = "ssh -t matt@100.64.0.9'tmux -2u new -At phone'";
+
+          # Proxmox
           pve = "ssh -t matt@100.64.0.4 'tmux -2u new -At phone'";
 
+          # Proxmox LXC instances
           pod = "mosh matt@100.64.0.4 -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At phone'";
           jelly = "mosh matt@100.64.0.4 -- ssh -t matt@10.0.0.123 'tmux -2u new -At phone'";
           qbit = "mosh matt@100.64.0.4 -- ssh -t matt@10.0.0.128 'tmux -2u new -At phone'";
--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -14,13 +14,24 @@ in {
 
   home-manager.users.${mainUser} = {
     programs.bash.shellAliases = {
+      # Connect to headscale
       tup = "tailscale up --login-server https://headscale.nelim.org";
 
+      # Desktop
       pc = "ssh -t matt@binto 'tmux -2u new -At ${hostName}'";
+
+      # Misc Nix servers
       oksys = "ssh -t matt@oksys 'tmux -2u new -At ${hostName}'";
       servivi = "ssh -t matt@servivi 'tmux -2u new -At ${hostName}'";
+
+      # Cluster nodes
+      thingone = "ssh -t matt@thingone 'tmux -2u new -At ${hostName}'";
+      thingtwo = "ssh -t matt@thingtwo 'tmux -2u new -At ${hostName}'";
+
+      # Proxmox
       pve = "ssh -t matt@pve 'tmux -2u new -At ${hostName}'";
 
+      # Proxmox LXC instances
       pod = "mosh matt@pve -- ssh -t -p 6768 matt@10.0.0.122 'tmux -2u new -At ${hostName}'";
       jelly = "mosh matt@pve -- ssh -t matt@10.0.0.123 'tmux -2u new -At ${hostName}'";
       qbit = "mosh matt@pve -- ssh -t matt@10.0.0.128 'tmux -2u new -At ${hostName}'";
