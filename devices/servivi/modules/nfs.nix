6dc386046b2a9729c35ad9d8261d61959abf86dc
matt1432 <matt@nelim.org>
2024-01-23 23:23:56 -0500

    feat(servivi): add headscale nfs share

--- devices/servivi/modules/nfs.nix
+++ devices/servivi/modules/nfs.nix
@@ -6,8 +6,9 @@
     createMountPoints = true;
 
     exports = ''
-      /export         10.0.0.244(rw,crossmnt,fsid=0,no_root_squash) 10.0.0.159(rw,crossmnt,fsid=0,no_root_squash)
-      /export/caddy   10.0.0.244(rw,nohide,no_root_squash) 10.0.0.159(rw,nohide,no_root_squash)
+      /export             10.0.0.244(rw,crossmnt,fsid=0,no_root_squash) 10.0.0.159(rw,crossmnt,fsid=0,no_root_squash) 100.64.0.8(rw,crossmnt,fsid=0,no_root_squash) 100.64.0.9(rw,crossmnt,fsid=0,no_root_squash)
+      /export/caddy       10.0.0.244(rw,nohide,no_root_squash) 10.0.0.159(rw,nohide,no_root_squash) 100.64.0.8(rw,nohide,no_root_squash) 100.64.0.9(rw,nohide,no_root_squash)
+      /export/headscale   10.0.0.244(rw,nohide,no_root_squash) 10.0.0.159(rw,nohide,no_root_squash) 100.64.0.8(rw,nohide,no_root_squash) 100.64.0.9(rw,nohide,no_root_squash)
     '';
   };
 }
