aed252651f2a794e93fbe96c4cd00a050b0c5ca0
matt1432 <matt@nelim.org>
2024-02-21 14:41:15 -0500

    fix(portals): update config to latest breaking changes

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -25,7 +25,13 @@ in {
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
+    xdgOpenUsePortal = true;
+
+    config = {
+      common.default = ["gtk"];
+      hyprland.default = ["gtk" "hyprland"];
+    };
+
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
     ];
