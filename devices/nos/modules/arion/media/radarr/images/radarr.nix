f8ceddcabc64bae99ee32c6a9b5caaf09cc695f7
matt1432 <matt@nelim.org>
2024-03-18 15:14:32 -0400

    chore: update flake.lock

--- devices/nos/modules/arion/media/radarr/images/radarr.nix
+++ devices/nos/modules/arion/media/radarr/images/radarr.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage {
   imageName = "ghcr.io/linuxserver/radarr";
-  imageDigest = "sha256:ea775bc8c1911ff5b51c6a1199c266032e6fd406d28199b2f188b6073c6b1c64";
-  sha256 = "1jr8yl8bk21ahcydfsxk5mlzqhapx8yzkh9h4qcmzrg50xdq0y9i";
+  imageDigest = "sha256:d5cd4924a8806c5dac024b018ad902d4e426fbab7f72c53e06e448207d9f4bd1";
+  sha256 = "0j099ym5fzahfp02bic39swj0ysj0abv1885y92c6fn7mmhk5b28";
   finalImageName = "ghcr.io/linuxserver/radarr";
   finalImageTag = "latest";
 }
