d98290a6e4b856d3fd7a80cd445ac29cb55bd8ce
matt1432 <matt@nelim.org>
2024-04-19 11:54:21 -0400

    fix(gtk-lock): add deps to get types right

--- modules/ags/gtk-session-lock-types/default.nix
+++ modules/ags/gtk-session-lock-types/default.nix
@@ -1,6 +1,12 @@
 {
-  gtk-lock,
+  atk,
   buildNpmPackage,
+  gdk-pixbuf,
+  gobject-introspection,
+  gtk3,
+  gtk-lock,
+  harfbuzz,
+  pango,
   ...
 }:
 buildNpmPackage {
@@ -13,6 +19,14 @@ buildNpmPackage {
   dontNpmBuild = true;
 
   installPhase = ''
-    npx @ts-for-gir/cli generate -g '${gtk-lock.dev}/share/gir-1.0' -o $out
+    npx @ts-for-gir/cli generate ${builtins.concatStringsSep " " [
+      "-g ${gtk-lock.dev}/share/gir-1.0"
+      "-g ${gobject-introspection.dev}/share/gir-1.0"
+      "-g ${gtk3.dev}/share/gir-1.0"
+      "-g ${pango.dev}/share/gir-1.0"
+      "-g ${gdk-pixbuf.dev}/share/gir-1.0"
+      "-g ${harfbuzz.dev}/share/gir-1.0"
+      "-g ${atk.dev}/share/gir-1.0"
+    ]} -o $out
   '';
 }
