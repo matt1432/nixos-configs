0665c3bfae93a3313d657551f3420a610223084f
matt1432 <matt@nelim.org>
2024-05-01 10:21:28 -0400

    chore(forgejo): v7.0.0 -> v.7.0.1

--- devices/nos/modules/arion/forgejo/images/forgejo.nix
+++ devices/nos/modules/arion/forgejo/images/forgejo.nix
@@ -2,7 +2,7 @@ pkgs:
 pkgs.dockerTools.pullImage {
   imageName = "codeberg.org/forgejo/forgejo";
   imageDigest = "sha256:0b50b596246cc4c439b6113411973f4c0639cb8556c7cf98e8017efdb5c3ab90";
-  sha256 = "1jbnmdr7jdbwpmbl5gsp7p3yql1bd83fnmp3k1h6xymw7nigzflk";
+  sha256 = "0hw9qmhs5ykry51kc7cl1w1kcswrpza8dsam5b5samcb0ga609ng";
   finalImageName = "codeberg.org/forgejo/forgejo";
-  finalImageTag = "7";
+  finalImageTag = "7.0.1";
 }
