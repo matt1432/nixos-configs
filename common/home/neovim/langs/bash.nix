0667f0f4c92010d2f48b4bbfddc93b33e9b77a92
matt1432 <matt@nelim.org>
2024-05-09 09:13:40 -0400

    fix(nvim): keep bash settings even when not using as IDE

--- common/home/neovim/langs/bash.nix
+++ common/home/neovim/langs/bash.nix
@@ -6,33 +6,33 @@
 }: let
   inherit (config.vars) neovimIde;
   inherit (pkgs) vimPlugins;
-in
-  lib.mkIf neovimIde {
-    programs = {
-      # I love doing typos
-      bash.shellAliases = {
-        nivm = "nvim";
-        nivim = "nvim";
-      };
-
-      neovim = {
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
-
-        extraLuaConfig =
-          /*
-          lua
-          */
-          ''
-            vim.api.nvim_create_autocmd("FileType", {
-               pattern = 'sh',
-               command = 'setlocal ts=4 sw=4 sts=0 expandtab',
-            });
-          '';
-
-        plugins = [
-        ];
-      };
+in {
+  programs = {
+    # I love doing typos
+    bash.shellAliases = {
+      nivm = "nvim";
+      nivim = "nvim";
     };
-  }
+
+    neovim = {
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+
+      extraLuaConfig =
+        /*
+        lua
+        */
+        ''
+          vim.api.nvim_create_autocmd("FileType", {
+             pattern = 'sh',
+             command = 'setlocal ts=4 sw=4 sts=0 expandtab',
+          });
+        '';
+
+      plugins =
+        lib.mkIf neovimIde [
+        ];
+    };
+  };
+}
