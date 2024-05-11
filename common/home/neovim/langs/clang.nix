4f810126345add49a8ecb108d9e1028bd9ab439a
matt1432 <matt@nelim.org>
2024-05-11 15:33:38 -0400

    feat(nvim): add cmake and clangd lsp

--- common/home/neovim/langs/clang.nix
+++ common/home/neovim/langs/clang.nix
@@ -13,9 +13,30 @@ in
         extraPackages = with pkgs; [
           gcc
           clang-tools
+          cmake-language-server
         ];
 
+        extraLuaConfig =
+          /*
+          lua
+          */
+          ''
+            local lsp = require('lspconfig');
+            local coq = require('coq');
+
+            lsp.cmake.setup(coq.lsp_ensure_capabilities({}));
+
+            lsp.clangd.setup(coq.lsp_ensure_capabilities({
+                handlers = require('lsp-status').extensions.clangd.setup(),
+                on_attach = function(_, bufnr)
+                    require("clangd_extensions.inlay_hints").setup_autocmd()
+                    require("clangd_extensions.inlay_hints").set_inlay_hints()
+                end,
+            }));
+          '';
+
         plugins = [
+          vimPlugins.clangd_extensions-nvim
         ];
       };
     };
