a6c90f335c2b1dd793a29352410b5da2bb9930e0
matt1432 <matt@nelim.org>
2024-05-22 10:24:21 -0400

    feat(nvidia): use latest beta driver

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -65,26 +65,15 @@ in {
       package =
         if !cfg.enableWayland
         then config.boot.kernelPackages.nvidiaPackages.stable
-        else let
-          rcu_patch = pkgs.fetchpatch {
-            url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-            hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-          };
-        in
-          # Keep the driver version at 535.xx.xx for Wayland desktop
-          # games stutter on more recent versions
-          # https://github.com/NixOS/nixpkgs/blob/e256f39bec8e01808c0a3e411d961cbced3f4e09/pkgs/os-specific/linux/nvidia-x11/default.nix#L70
-          config.boot.kernelPackages.nvidiaPackages.mkDriver rec {
-            version = "535.43.28";
-            persistencedVersion = "535.98";
-            settingsVersion = "535.98";
-            sha256_64bit = "sha256-ic7r3MPp65fdEwqDRyc0WiKonL5eF6KZUpfD/C3vYaU=";
-            openSha256 = "sha256-a5iccyISHheOfTwpsrz6puqrVhgzYWFvNlykVG3+PVc=";
-            settingsSha256 = "sha256-jCRfeB1w6/dA27gaz6t5/Qo7On0zbAPIi74LYLel34s=";
-            persistencedSha256 = "sha256-WviDU6B50YG8dO64CGvU3xK8WFUX8nvvVYm/fuGyroM=";
-            url = "https://developer.nvidia.com/downloads/vulkan-beta-${lib.concatStrings (lib.splitVersion version)}-linux";
-
-            patches = [rcu_patch];
+        else
+          # Get newest beta driver version for better Wayland support
+          config.boot.kernelPackages.nvidiaPackages.mkDriver {
+            version = "555.42.02";
+            sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+            sha256_aarch64 = "sha256-ekx0s0LRxxTBoqOzpcBhEKIj/JnuRCSSHjtwng9qAc0=";
+            openSha256 = "sha256-3/eI1VsBzuZ3Y6RZmt3Q5HrzI2saPTqUNs6zPh5zy6w=";
+            settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+            persistencedSha256 = "sha256-3ae31/egyMKpqtGEqgtikWcwMwfcqMv2K4MVFa70Bqs=";
           };
     };
 
