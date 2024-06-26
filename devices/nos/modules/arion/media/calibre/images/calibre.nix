c928cc2a86d623bcbbe394fca78ec253dfbbb949
matt1432 <matt@nelim.org>
2024-06-26 10:42:46 -0400

    chore: update flake.lock

--- devices/nos/modules/arion/media/calibre/images/calibre.nix
+++ devices/nos/modules/arion/media/calibre/images/calibre.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage {
   imageName = "ghcr.io/linuxserver/calibre";
-  imageDigest = "sha256:7cef47ba7fd105e2352b48fcff50c43a78415511beee2f742000cf9de5609331";
-  sha256 = "1lx3bcffqs0dn55826xx4jdysa36g7dy5hakhazfyxzv7wixkh62";
+  imageDigest = "sha256:ebfeb487977b5ffa7698b424b83002868521b65734d149aa4bf36e268709de2a";
+  sha256 = "0lvf6jf48anryrw6ns76g7xgvaab9bwh2d9xplaq6522dkz958vg";
   finalImageName = "ghcr.io/linuxserver/calibre";
   finalImageTag = "latest";
 }
--- devices/nos/modules/arion/media/prowlarr/images/flaresolverr.nix
+++ devices/nos/modules/arion/media/prowlarr/images/flaresolverr.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage {
   imageName = "ghcr.io/flaresolverr/flaresolverr";
-  imageDigest = "sha256:f44f6955dc7a4e3b4dd4b07e56217b445d19287095744d06d63f84988fe76b6b";
-  sha256 = "1303vgjlczc45ix7cnb1smghkxbnhkr7j6mnvpz84hm388rlalna";
+  imageDigest = "sha256:f104ee51e5124d83cf3be9b37480649355d223f7d8f9e453d0d5ef06c6e3b31b";
+  sha256 = "1ixsv5rv39bp7y1w2p0zjz9ivl82s3kndr5r39krgyknhzxavazc";
   finalImageName = "ghcr.io/flaresolverr/flaresolverr";
   finalImageTag = "latest";
 }
