7e7dde4c6f38dd97672795f34c1811bcf6cebc89
matt1432 <matt@nelim.org>
2024-06-29 17:57:50 -0400

    feat(servers): add config for 7Days server

--- devices/servivi/modules/steam-servers/default.nix
+++ devices/servivi/modules/steam-servers/default.nix
@@ -13,6 +13,15 @@
       config = {
         ServerName = "bruh moment";
         ServerPort = 26900;
+
+        BlockDamagePlayer = 200;
+        BloodMoonEnemyCount = 10;
+        DropOnDeath = 3;
+        PartySharedKillRange = 10000;
+        PlayerKillingMode = 2;
+        XPMultiplier = 200;
+        ZombieBMMove = 1;
+        ZombieMoveNight = 0;
       };
     };
   };
