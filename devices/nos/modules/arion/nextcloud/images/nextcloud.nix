9a7659814cf74161005af6d327604874b0af406a
matt1432 <matt@nelim.org>
2024-07-01 00:27:07 -0400

    chore: bump nextcloud

--- devices/nos/modules/arion/nextcloud/images/nextcloud.nix
+++ devices/nos/modules/arion/nextcloud/images/nextcloud.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage {
   imageName = "nextcloud";
-  imageDigest = "sha256:0bb498026da9fee4c7c703a9ed682aa2aee825a82946c6944a872cbedb44a272";
-  sha256 = "1ahpz4a1pdbg7554ydqzfagra32snd6pc7dv1afq5pwyd03cllr4";
+  imageDigest = "sha256:7df7de6e9b7438839f1f7519ae345015bda742754b65d90659d0214bfe07c00b";
+  sha256 = "1kg54w12ja78cm33hvj0c21wlmx7mdg7rcwb7r75ig46w175p9v2";
   finalImageName = "nextcloud";
-  finalImageTag = "29.0.1-fpm";
+  finalImageTag = "29.0.3-fpm";
 }
