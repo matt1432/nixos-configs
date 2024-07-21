322aeebd4572630f09d1274dddfd7376b008d6c3
matt1432 <matt@nelim.org>
2024-07-20 22:33:14 -0400

    feat(update): migrate vuetorrent update script to typescript

--- apps/update/src/app.ts
+++ apps/update/src/app.ts
@@ -1,19 +1,24 @@
 import { parseArgs } from './lib.ts';
 import { updateFirefoxAddons } from '././firefox.ts';
-import { updateDocker } from './misc.ts';
+import { updateDocker, updateVuetorrent } from './misc.ts';
 
 
 const args = parseArgs();
-
-if (args['f'] || args['firefox']) {
-    console.log(updateFirefoxAddons());
-}
 
 if (args['d'] || args['docker']) {
     console.log(updateDocker());
 }
 
+if (args['f'] || args['firefox']) {
+    console.log(updateFirefoxAddons());
+}
+
+if (args['v'] || args['vuetorrent']) {
+    console.log(updateVuetorrent());
+}
+
 if (args['a'] || args['all']) {
+    console.log(updateVuetorrent());
     console.log(updateFirefoxAddons());
     console.log(updateDocker());
 }
