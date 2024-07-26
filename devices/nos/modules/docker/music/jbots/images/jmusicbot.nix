3923dcb60446daa0fafe0fdc0be3fb8b4482734f
matt1432 <matt@nelim.org>
2024-07-25 23:29:11 -0400

    fix(servers): update jmusicbots to latest

--- devices/nos/modules/docker/music/jbots/images/jmusicbot.nix
+++ devices/nos/modules/docker/music/jbots/images/jmusicbot.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage {
   imageName = "craumix/jmusicbot";
-  imageDigest = "sha256:f913923899a29ede4a9a5dc695b40a35872b144dfc6fe78f7dac25f55976c144";
-  sha256 = "07i9c8ah37zp704grcjhhl252k99i1pzfs3pa6fnzd1cbzbamwb6";
+  imageDigest = "sha256:0afa4efbd43a718fba9108cdab8865f80bb1690f690920e732303d081507868d";
+  sha256 = "1nw7z1ypl7k4kkxrxa45skjb2jrn4yxq13bwpadry4lsfcpqi5z9";
   finalImageName = "craumix/jmusicbot";
-  finalImageTag = "0.3.9";
+  finalImageTag = "latest";
 }
