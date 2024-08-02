e1dd7909e00cdb63d599e0aadb0819300b313661
matt1432 <matt@nelim.org>
2024-08-02 00:08:30 -0400

    feat(nvim): change cmp binds

--- common/home/neovim/plugins/cmp.lua
+++ common/home/neovim/plugins/cmp.lua
@@ -3,14 +3,11 @@ local has_words_before = function()
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil;
 end;
 
-local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true);
-end;
-
 local cmp = require('cmp');
 
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
 cmp.event:on(
   'confirm_done',
   cmp_autopairs.on_confirm_done()
@@ -33,12 +30,13 @@ cmp.setup({
     },
 
     mapping = {
+        -- Confirm selection
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
+
+        -- Next selection
+        ['<Down>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item();
-            elseif vim.fn['vsnip#available'](1) == 1 then
-                feedkey('<Plug>(vsnip-expand-or-jump)', '');
             elseif has_words_before() then
                 cmp.complete();
             else
@@ -49,11 +47,10 @@ cmp.setup({
             's',
         }),
 
-        ['<S-Tab>'] = cmp.mapping(function()
+        -- Previous selection
+        ['<Up>'] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item();
-            elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-                feedkey('<Plug>(vsnip-jump-prev)', '');
             end;
         end, {
             'i',
