e65fdd95eb1eb440a3666d45badad1258df50d76
matt1432 <matt@nelim.org>
2024-09-10 17:04:30 -0400

    feat(bluetooth): add script to turn on UE Boom speaker

--- devices/homie/modules/home-assistant/bluetooth.nix
+++ devices/homie/modules/home-assistant/bluetooth.nix
@@ -1,4 +1,27 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "turnOnUE";
+
+      runtimeInputs = [config.hardware.bluetooth.package];
+
+      text = ''
+        cmd=0x0003
+        bt_device_addr=88:C6:26:93:4B:77
+
+        # This is the MAC address on the first line of `bluetootctl show`
+        # without the `:` and with `01` added at the end
+        bt_controller_addr=E848B8C8200001
+
+        exec gatttool -b $bt_device_addr --char-write-req --handle=$cmd --value=$bt_controller_addr
+      '';
+    })
+  ];
+
   # Setup Bluetooth
   hardware.bluetooth = {
     enable = true;
