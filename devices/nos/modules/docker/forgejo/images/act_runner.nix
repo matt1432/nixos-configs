7391427c2932a375dbc9391f1afdec84362cac70
matt1432 <matt@nelim.org>
2024-10-30 19:45:43 -0400

    chore: update flake.lock
    Flake Inputs:
    • Updated input 'astal':
        'github:Aylur/astal/c1eb257' (2024-10-29)
      → 'github:Aylur/astal/2585ca7' (2024-10-30)
    
    • Updated input 'gtk-theme-src':
        'github:dracula/gtk/a698492' (2024-10-21)
      → 'github:dracula/gtk/9330b7f' (2024-10-30)
    
    • Updated input 'hyprgrass':
        'github:horriblename/hyprgrass/4045c9f' (2024-10-27)
      → 'github:horriblename/hyprgrass/f97b6ac' (2024-10-30)
    
    • Updated input 'hyprland':
        'github:hyprwm/Hyprland/d679d20' (2024-10-28)
      → 'github:hyprwm/Hyprland/7c7a84f' (2024-10-30)
    
    • Updated input 'nix-fast-build':
        'github:Mic92/nix-fast-build/1775c73' (2024-10-07)
      → 'github:Mic92/nix-fast-build/8e7c9d7' (2024-10-30)
    
    • Updated input 'nixpkgs-wayland':
        'github:nix-community/nixpkgs-wayland/d3bb51e' (2024-10-29)
      → 'github:nix-community/nixpkgs-wayland/35f40f1' (2024-10-30)
    
    • Updated input 'spotifyplus-src':
        'github:thlucas1/homeassistantcomponent_spotifyplus/f4ab736' (2024-10-27)
      → 'github:thlucas1/homeassistantcomponent_spotifyplus/392f331' (2024-10-30)
    
    • Updated input 'spotifywebapi-src':
        'github:thlucas1/SpotifyWebApiPython/8c6aff1' (2024-10-19)
      → 'github:thlucas1/SpotifyWebApiPython/4d3b7b5' (2024-10-30)
    
    • Updated input 'tuya-local-src':
        'github:make-all/tuya-local/351e576' (2024-10-29)
      → 'github:make-all/tuya-local/bbd4179' (2024-10-30)
    
    Docker Images:
    • vegardit/gitea-act-runner dind-latest:
       sha256:fe6f44905830a17f404e9912e2c4fe2f4a2c47bb3ee35409da6476618cf9dab5
     → sha256:ea8e9a0304fba7648b62fe19c39c38dd08e8c71b8d2a0cac65f834ccf6215daf

--- devices/nos/modules/docker/forgejo/images/act_runner.nix
+++ devices/nos/modules/docker/forgejo/images/act_runner.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage rec {
   imageName = "vegardit/gitea-act-runner";
-  imageDigest = "sha256:fe6f44905830a17f404e9912e2c4fe2f4a2c47bb3ee35409da6476618cf9dab5";
-  sha256 = "0jci209g7qqsqa1qi87fjj00q1yz8d4wj7gqsy4la8jjrkb23arg";
+  imageDigest = "sha256:ea8e9a0304fba7648b62fe19c39c38dd08e8c71b8d2a0cac65f834ccf6215daf";
+  sha256 = "1x2vnnz38jivrk4wmqymxlhmaz0vasq81r6p9zlfr7pwj8hysj4m";
   finalImageName = imageName;
   finalImageTag = "dind-latest";
 }
