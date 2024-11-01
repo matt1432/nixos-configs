3039f823c90876556d20bccff489b46dfbec348c
matt1432 <matt@nelim.org>
2024-11-01 18:24:03 -0400

    chore(agsV2): don't print hyprpaper error

--- nixosModules/ags-v2/config/configurations/wim.ts
+++ nixosModules/ags-v2/config/configurations/wim.ts
@@ -32,7 +32,7 @@ export default async() => {
         },
 
         main: () => {
-            execAsync('hyprpaper').catch(print);
+            execAsync('hyprpaper').catch(() => { /**/ });
 
             AppLauncher();
             Bar();
