9c01cbd74c316e50200c543aa304a0036a23d0e5
matt1432 <matt@nelim.org>
2024-12-05 19:32:36 -0500

    chore: update immich to latest

--- devices/nos/modules/docker/immich/images/machine-learning.nix
+++ devices/nos/modules/docker/immich/images/machine-learning.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage rec {
   imageName = "ghcr.io/immich-app/immich-machine-learning";
-  imageDigest = "sha256:1b8494bb9fe2194f2dc72c4d6b0104e16718f50e8772d54ade57909770816ad1";
-  sha256 = "12l0h6yjj53sqfjm7ik4ss87lsjlb85xkrj7l25pkcfgm5i8qqdx";
+  imageDigest = "sha256:c52a1eacd797d94461b11336655595e2083524ecd2e513a8fb9d9d83ccbede94";
+  sha256 = "1irnwqg8qibyigv6xwwsfgbhlfjs5ys93mrwws61c7hi21pisv5h";
   finalImageName = imageName;
   finalImageTag = "release";
 }
--- devices/nos/modules/docker/immich/images/server.nix
+++ devices/nos/modules/docker/immich/images/server.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage rec {
   imageName = "ghcr.io/immich-app/immich-server";
-  imageDigest = "sha256:851c02f28891f1854c5b5762ee8d2e254e2de528cfe3627b2fbcb37a7f108ff3";
-  sha256 = "02x48335crrvw1981wy3cp4668jl8r7qddz7jjr87yiisk0jvx58";
+  imageDigest = "sha256:20b5b6a2845e40ff18cc3b3ae7d9261638f40511ea73c3c9ececd428e95fb43e";
+  sha256 = "11v6kk7jgp98j7zd7lp714ci1xqhkklvyxj4ssapwplxggkbh4yl";
   finalImageName = imageName;
   finalImageTag = "release";
 }
