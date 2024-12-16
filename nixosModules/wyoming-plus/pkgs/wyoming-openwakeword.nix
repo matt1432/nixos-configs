ce9bb61f7fc1482cf3315b322ed76e543c5816ad
matt1432 <matt@nelim.org>
2024-12-15 21:50:55 -0500

    fix(wyoming): get rid of onnx error

--- nixosModules/wyoming-plus/pkgs/wyoming-openwakeword.nix
+++ nixosModules/wyoming-plus/pkgs/wyoming-openwakeword.nix
@@ -3,13 +3,14 @@ This package uses a `wyoming-openwakeword` fork that makes use of
 the upstream `openwakeword` instead of a fork: https://github.com/rhasspy/wyoming-openwakeword/pull/27
 */
 {
-  fetchFromGitHub,
   lib,
+  fetchFromGitHub,
+  onnxruntime,
   openwakeword,
   python3Packages,
   ...
 }: let
-  inherit (lib) attrValues;
+  inherit (lib) attrValues makeLibraryPath;
 in
   python3Packages.buildPythonApplication {
     pname = "wyoming-openwakeword";
@@ -19,7 +20,6 @@ in
     src = fetchFromGitHub {
       owner = "rhasspy";
       repo = "wyoming-openwakeword";
-      # rev = "synesthesiam-20240627-openwakeword";
       rev = "324d669645a778439c5392d9e287a763ead3cf4c";
       hash = "sha256-69oR2LHiUfx8j39nWp7XhG5xTvmOoPCLjSlH1CFvavo=";
     };
@@ -46,6 +46,11 @@ in
 
     pythonImportsCheck = [
       "wyoming_openwakeword"
+    ];
+
+    # Native onnxruntime lib used by Python module onnxruntime can't find its other libs without this
+    makeWrapperArgs = [
+      ''--prefix LD_LIBRARY_PATH : "${makeLibraryPath [onnxruntime]}"''
     ];
 
     meta = {
