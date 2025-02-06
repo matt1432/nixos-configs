a504422ca8c07af7c5fdc1cd1062ecde4c3100c1
matt1432 <matt@nelim.org>
2025-02-06 18:17:55 -0500

    feat(ags): add message when shutting down, rebooting, etc.

--- modules/ags/config/widgets/powermenu/index.tsx
+++ modules/ags/config/widgets/powermenu/index.tsx
@@ -1,38 +1,62 @@
 import { execAsync } from 'astal';
-import { Astal } from 'astal/gtk3';
+import { Astal, Gtk } from 'astal/gtk3';
 
 import { hyprMessage } from '../../lib';
 
 import PopupWindow from '../misc/popup-window';
 
 
-const PowermenuWidget = () => (
-    <centerbox className="powermenu widget">
-        <button
-            className="shutdown button"
-            cursor="pointer"
-            onButtonReleaseEvent={() => execAsync(['systemctl', 'poweroff']).catch(print)}
-        >
-            <icon icon="system-shutdown-symbolic" />
-        </button>
+const PowermenuWidget = () => {
+    const label = <label /> as Astal.Label;
 
-        <button
-            className="reboot button"
-            cursor="pointer"
-            onButtonReleaseEvent={() => execAsync(['systemctl', 'reboot']).catch(print)}
-        >
-            <icon icon="system-restart-symbolic" />
-        </button>
+    return (
+        <centerbox className="powermenu widget">
+            <stack transitionType={Gtk.StackTransitionType.SLIDE_DOWN}>
+                <box name="buttons">
+                    <button
+                        className="shutdown button"
+                        cursor="pointer"
+                        onButtonReleaseEvent={(self) => {
+                            label.set_label('Shutting down...');
+                            (self.get_parent()?.get_parent() as Astal.Stack).set_shown('message');
+                            execAsync(['systemctl', 'poweroff']).catch(print);
+                        }}
+                    >
+                        <icon icon="system-shutdown-symbolic" />
+                    </button>
 
-        <button
-            className="logout button"
-            cursor="pointer"
-            onButtonReleaseEvent={() => hyprMessage('dispatch exit').catch(print)}
-        >
-            <icon icon="system-log-out-symbolic" />
-        </button>
-    </centerbox>
-);
+                    <button
+                        className="reboot button"
+                        cursor="pointer"
+                        onButtonReleaseEvent={(self) => {
+                            label.set_label('Rebooting...');
+                            (self.get_parent()?.get_parent() as Astal.Stack).set_shown('message');
+                            execAsync(['systemctl', 'reboot']).catch(print);
+                        }}
+                    >
+                        <icon icon="system-restart-symbolic" />
+                    </button>
+
+                    <button
+                        className="logout button"
+                        cursor="pointer"
+                        onButtonReleaseEvent={(self) => {
+                            label.set_label('Logging out...');
+                            (self.get_parent()?.get_parent() as Astal.Stack).set_shown('message');
+                            hyprMessage('dispatch exit').catch(print);
+                        }}
+                    >
+                        <icon icon="system-log-out-symbolic" />
+                    </button>
+                </box>
+
+                <box name="message" hexpand={false} halign={Gtk.Align.CENTER}>
+                    {label}
+                </box>
+            </stack>
+        </centerbox>
+    );
+};
 
 export default () => (
     <PopupWindow
