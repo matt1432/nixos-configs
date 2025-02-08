715afda72893e3e83c93a63e9119f9e425d80001
matt1432 <matt@nelim.org>
2025-02-08 17:20:30 -0500

    chore: remove unneeded overlays

--- overlays/misc-fixes/default.nix
+++ overlays/misc-fixes/default.nix
@@ -1,6 +1,1 @@
-final: prev: {
-  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/wy/wyoming-faster-whisper/package.nix
-  wyoming-faster-whisper = prev.wyoming-faster-whisper.overridePythonAttrs (o: {
-    meta = {mainProgram = o.pname;} // o.meta;
-  });
-}
+final: prev: {}
