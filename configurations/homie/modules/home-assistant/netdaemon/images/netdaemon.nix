61de6f90115519da2641adc7d2e6412617bb44af
matt1432 <matt@nelim.org>
2025-04-13 14:56:01 -0400

    chore: bump netdaemon 25.10.0 -> 25.14.0

--- configurations/homie/modules/home-assistant/netdaemon/images/netdaemon.nix
+++ configurations/homie/modules/home-assistant/netdaemon/images/netdaemon.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage rec {
   imageName = "netdaemon/netdaemon5";
-  imageDigest = "sha256:3290c3a8112e0abb076d9a16e1b20e31c5a97e425cb4578e7406007f443dccb5";
-  hash = "sha256-W/MlJejTpHsJfF+JyUKYKFoSUhvSS827IWpjYZQ0Tj0=";
+  imageDigest = "sha256:f1685239cd48f3d728f393d86975f954a954e2f64bad197cb534a66fafaddd55";
+  hash = "sha256-KF6QLKuO8O2jE70dOprE8lfCnXvbhfk2XYp76pGXujE=";
   finalImageName = imageName;
-  finalImageTag = "25.10.0";
+  finalImageTag = "25.14.0";
 }
