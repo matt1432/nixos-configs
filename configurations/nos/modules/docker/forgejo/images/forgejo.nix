354ea274c8b2c7b794dc00efcbefc5e89178fd6f
matt1432 <matt@nelim.org>
2025-04-18 17:16:50 -0400

    chore: bump forgejo 10 -> 11

--- configurations/nos/modules/docker/forgejo/images/forgejo.nix
+++ configurations/nos/modules/docker/forgejo/images/forgejo.nix
@@ -1,8 +1,8 @@
 pkgs:
 pkgs.dockerTools.pullImage rec {
   imageName = "codeberg.org/forgejo/forgejo";
-  imageDigest = "sha256:99b6c15a1bc98e623103a83a04023662a93fd035dac4f0a856d781afa9d71095";
-  hash = "sha256-QRs4/j9sAWTeSfA8vTm7eSVZBEsFMT34UEu/7RtriL4=";
+  imageDigest = "sha256:e7663b58cc0a1610f31b315150e4fa8a00f6936d98abd6e26ef247f009cc7f52";
+  hash = "sha256-wqXqOYhXkY4F4aQ2I6o1c6FZ/VyarJaur/rELD9v3iA=";
   finalImageName = imageName;
-  finalImageTag = "10";
+  finalImageTag = "11";
 }
