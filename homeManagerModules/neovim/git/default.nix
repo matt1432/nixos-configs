6380c0b42626479835c3014a4214d986bf75035a
matt1432 <matt@nelim.org>
2025-06-01 12:39:02 -0400

    fix(nvim): use latest version og gitsigns

--- homeManagerModules/neovim/git/default.nix
+++ homeManagerModules/neovim/git/default.nix
@@ -13,11 +13,21 @@ in {
       pkgs.vimPlugins.fugitive
 
       {
-        plugin = pkgs.vimPlugins.gitsigns-nvim;
+        # FIXME: wait for it to reach nixpkgs
+        plugin = pkgs.vimPlugins.gitsigns-nvim.overrideAttrs rec {
+          version = "1.0.2";
+          src = pkgs.fetchFromGitHub {
+            owner = "lewis6991";
+            repo = "gitsigns.nvim";
+            tag = "v${version}";
+            hash = "sha256-qWusbKY+3d1dkW5oLYDyfSLdt1qFlJdDeXgFWqQ4hUI=";
+          };
+        };
         type = "lua";
         config =
           # lua
           ''
+            --
             local gitsigns = require("gitsigns");
 
             vim.keymap.set("v", "gs", function()
