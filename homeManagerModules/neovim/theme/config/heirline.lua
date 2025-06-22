2bb2e4889b8493534d16de052f21fb9bd824c90b
matt1432 <matt@nelim.org>
2025-06-22 15:53:57 -0400

    fix(nvim): get rid of heirline crash with css

--- homeManagerModules/neovim/theme/config/heirline.lua
+++ homeManagerModules/neovim/theme/config/heirline.lua
@@ -133,17 +133,14 @@ local FileIcon = {
     init = function(self)
         local filename = self.filename;
         local extension = vim.fn.fnamemodify(filename, ':e');
-        self.icon, self.icon_color =
-            require('nvim-web-devicons').get_icon_color(filename, extension, { default = true });
+        self.icon, _ = require('nvim-web-devicons').get_icon(filename, extension, { default = true });
     end,
 
     provider = function(self)
         return self.icon and (self.icon .. ' ');
     end,
 
-    hl = function(self)
-        return { fg = self.icon_color };
-    end,
+    hl = { fg = utils.get_highlight('Directory').fg },
 };
 
 local FileName = {
