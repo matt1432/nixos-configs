a61168839089f237152a5c712ebd81eb26235ca0
matt1432 <matt@nelim.org>
2025-08-04 15:16:44 -0400

    fix(bbsteamie): build kde 6.3.1 from current nixpkgs

--- configurations/bbsteamie/modules/desktop/default.nix
+++ configurations/bbsteamie/modules/desktop/default.nix
@@ -8,19 +8,46 @@
 
   kde63Pkgs = import nixpkgs-kde63 {
     inherit (pkgs) system cudaSupport;
-    config.allowUnfree = true;
   };
 in {
-  # -= KDE 6.3.0
+  # -= KDE 6.3.1
   disabledModules = ["${nixpkgs}/nixos/modules/services/desktop-managers/plasma6.nix"];
   nixpkgs.overlays = [
     (final: prev: {
-      inherit (kde63Pkgs) kdePackages;
+      # KRDP requires specific FreeRDP version
+      inherit (kde63Pkgs) freerdp;
+
+      # Fixes plasma-workspace and other calls to substituteAll
+      substituteAll = attrs:
+        final.replaceVars attrs.src (
+          (builtins.removeAttrs attrs ["src"]) // {QtBinariesDir = null;}
+        );
+
+      kdePackages = (final.callPackage "${nixpkgs-kde63}/pkgs/kde" {}).overrideScope (
+        kdeFinal: kdePrev: {
+          threadweaver = kdePrev.threadweaver.overrideAttrs (o: {
+            postPatch = ''
+              ${o.postPatch or ""}
+              substituteInPlace ./CMakeLists.txt --replace-fail \
+                  'add_subdirectory(examples)' ""
+            '';
+          });
+
+          inherit
+            (prev.kdePackages)
+            breeze
+            kdeconnect-kde
+            ktextaddons
+            kpimtextedit
+            konsole
+            ;
+        }
+      );
     })
   ];
   imports = [
     "${nixpkgs-kde63}/nixos/modules/services/desktop-managers/plasma6.nix"
-    # KDE 6.3.0 =-
+    # KDE 6.3.1 =-
 
     (import ./session-switching.nix defaultSession)
     (import ./steam.nix defaultSession)
