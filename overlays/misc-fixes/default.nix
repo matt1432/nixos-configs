6b2f0a0383455e99a777b204ddf8056ed244e83c
matt1432 <matt@nelim.org>
2025-09-15 08:19:21 -0400

    fix(whoogle): bump version to 0.9.4

--- overlays/misc-fixes/default.nix
+++ overlays/misc-fixes/default.nix
@@ -3,4 +3,14 @@ final: prev: {
   spotifywm = prev.spotifywm.overrideAttrs (o: {
     buildInputs = o.buildInputs ++ [final.libxcb];
   });
+
+  # FIXME: remove when it reaches nixpkgs
+  whoogle-search = prev.whoogle-search.overridePythonAttrs (o: rec {
+    version = "0.9.4";
+    src = final.fetchPypi {
+      pname = "whoogle_search";
+      inherit version;
+      hash = "sha256-EvmNDU1hRUIy+CTwECLzIdcEjzcJgiiFYd2iMy0wDG0=";
+    };
+  });
 }
