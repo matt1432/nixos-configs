2e8d1596c91adf7087854d310bf01b2a3ed9c2ff
matt1432 <matt@nelim.org>
2025-12-05 10:58:56 -0500

    refactor(proton-ge): make helper function

--- scopedPackages/proton-ge/default.nix
+++ scopedPackages/proton-ge/default.nix
@@ -3,26 +3,41 @@
   pkgs,
   ...
 }:
-lib.makeScope pkgs.newScope (ge: {
-  latest = pkgs.proton-ge-bin.override {
-    steamDisplayName = "GE-Proton-Latest";
-  };
-
-  v10-20 =
-    (pkgs.proton-ge-bin.overrideAttrs (o: rec {
-      version = "GE-Proton10-20";
-      src = pkgs.fetchzip {
-        url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
-        hash = "sha256-sJkaDEnfAuEqcLDBtAfU6Rny3P3lOCnG1DusWfvv2Fg=";
-      };
-      meta =
-        o
-        // {
-          description = ''
-            ${o.meta.description} This was the last working version for Marvel Rivals afaik.
-          '';
-        };
-    })).override {
-      steamDisplayName = "GE-Proton10-20";
+lib.makeScope pkgs.newScope (_: let
+  mkProtonVersion = {
+    versionSuffix ? "-Latest",
+    hash ? null,
+    desc ? null,
+  }:
+    (
+      if (versionSuffix != "-Latest")
+      then
+        (
+          pkgs.proton-ge-bin.overrideAttrs (o: rec {
+            version = "GE-Proton${versionSuffix}";
+            src = pkgs.fetchzip {
+              url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${version}/${version}.tar.gz";
+              inherit hash;
+            };
+            meta =
+              o
+              // {
+                description = ''
+                  ${o.meta.description} ${desc}
+                '';
+              };
+          })
+        )
+      else pkgs.proton-ge-bin
+    ).override {
+      steamDisplayName = "GE-Proton${versionSuffix}";
     };
+in {
+  latest = mkProtonVersion {};
+
+  v10-20 = mkProtonVersion {
+    versionSuffix = "10-20";
+    hash = "sha256-sJkaDEnfAuEqcLDBtAfU6Rny3P3lOCnG1DusWfvv2Fg=";
+    desc = "This was the last working version for Marvel Rivals afaik.";
+  };
 })
