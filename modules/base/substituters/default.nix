a0ac3bfa7c94a6653e785432ee4eb47e56778693
matt1432 <matt@nelim.org>
2026-01-25 14:51:23 -0500

    feat(darwin): add matt-darwin cache

--- modules/base/substituters/default.nix
+++ modules/base/substituters/default.nix
@@ -35,7 +35,8 @@ in {
           trusted-public-keys = optionals (key != null) [key];
         };
       in
-        mergeAttrsList ([
+        mergeAttrsList (
+          [
             (mkSubstituterConf 200 "https://cache.nixos.org" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=")
             (mkSubstituterConf 1000 "https://hyprland.cachix.org" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=")
             (mkSubstituterConf 1000 "https://nix-gaming.cachix.org" "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=")
@@ -45,9 +46,13 @@ in {
             (mkSubstituterConf 1000 "https://cuda-maintainers.cachix.org" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=")
             (mkSubstituterConf 1100 "https://nix-on-droid.cachix.org" "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU=")
           ]
+          ++ optionals isDarwin [
+            (mkSubstituterConf 100 "https://matt-darwin.cachix.org" "matt-darwin.cachix.org-1:yFHQVe96+ljXwv8oz9XSWt9Qz98aYCVF2xInTbKg7U8=")
+          ]
           ++ optionals (config.networking.hostName != "servivi") [
             (mkSubstituterConf 100 "https://cache.nelim.org" "cache.nelim.org:JmFqkUdH11EA9EZOFAGVHuRYp7EbsdJDHvTQzG2pPyY=")
-          ]);
+          ]
+        );
     };
   };
 
