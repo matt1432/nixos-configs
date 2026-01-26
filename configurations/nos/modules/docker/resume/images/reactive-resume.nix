4bde792809fc1e5884713fe99a811e0575fd9b01
matt1432 <matt@nelim.org>
2026-01-26 05:39:59 -0500

    fix(resume): pin image to v4

--- configurations/nos/modules/docker/resume/images/reactive-resume.nix
+++ configurations/nos/modules/docker/resume/images/reactive-resume.nix
@@ -1,8 +1,9 @@
 pkgs:
 pkgs.dockerTools.pullImage rec {
-  imageName = "amruthpillai/reactive-resume";
-  imageDigest = "sha256:da0bb7109bc3066a64c97d03d6e1f61c31eb51ae3d1c1ddefa6764c861680fd8";
-  hash = "sha256-2X/iRjYm//kp5cMDntJMEmLS93wb5LGElgAUQxnU7ts=";
+  imageName = "ghcr.io/amruthpillai/reactive-resume";
+  imageDigest = "sha256:f26fd329f35b97bccdf5ee5a7b558df0ccf1e073a1d7a5787e433043e22b9296";
+  hash = "sha256-qWcT+3l9MOAEDzfqEOmc7gfB0gkia5dJ3ziG/PMESgo=";
   finalImageName = imageName;
-  finalImageTag = "latest";
+  # FIXME: https://docs.rxresu.me/self-hosting/migration
+  finalImageTag = "v4.5.5";
 }
