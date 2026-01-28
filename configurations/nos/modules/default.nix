262ad46c08876ab4e28f16298c9c4a87826f1614
matt1432 <matt@nelim.org>
2026-01-28 11:45:57 -0500

    chore(nos): disable subtitles module for now

--- configurations/nos/modules/default.nix
+++ configurations/nos/modules/default.nix
@@ -12,6 +12,8 @@
     ./mergerfs
     ./qbittorrent
     ./snapraid
-    ./subtitles
+
+    # FIXME: deleted '/data/history/Apocalypse - Never-Ending War 1918-1926 (2018) [imdbid-tt8561666]'?
+    # ./subtitles
   ];
 }
