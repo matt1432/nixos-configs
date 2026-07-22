8fb84f5eea1bbdaa6fe31d9dd6f9ae6704ebc555
matt1432 <matt@nelim.org>
2026-07-22 13:15:27 -0400

    chore(docker): update joal hash

--- configurations/nos/modules/docker/media/joal/images/joal.nix
+++ configurations/nos/modules/docker/media/joal/images/joal.nix
@@ -2,7 +2,7 @@ pkgs:
 pkgs.dockerTools.pullImage rec {
   imageName = "anthonyraymond/joal";
   imageDigest = "sha256:fc942567c5649b29377e50aa0ca934e4750534d02b4ecf5a0eda3984630f35d7";
-  sha256 = "03wqqhpsjcdgr4q3n9vqyxb59324mxnwn8jn6kj2kb6zq8bz3qrj";
+  sha256 = "sha256-z/fEL8rYHu8bffND1tv/Nr/pSEHjnMuNekVyL26CCGA=";
   finalImageName = imageName;
   finalImageTag = "latest";
 }
